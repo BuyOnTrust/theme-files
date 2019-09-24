a494126b29e8eb78e53be87946f4597d14819c9b
Weston Gibler <weston@bluekeel.com>
2019-09-24 15:06:58 -0600

    Modal styling in the header looks accuarate

--- assets/js/theme/global/pre-preapproval.js
+++ assets/js/theme/global/pre-preapproval.js
@@ -19,7 +19,7 @@ export default function (context) {
         // Construction
         modalContent.appendChild(header);
         modalContent.appendChild(body);
-        modalContent.appendChild(footer);
+        // modalContent.appendChild(footer);
 
         // const modalEl = document.querySelector('#modal');
         // modalEl.style.maxHeight = '100vh';
@@ -57,10 +57,10 @@ export default function (context) {
         modalContainer.style.minHeight = '850px';
         modalContainer.style.padding = '80px 0';
         modalContainer.style.overflow = 'hidden';
-        modalContainer.style.margin = 'auto';        
+        modalContainer.style.margin = 'auto';
         return modalContainer;
     };
-
+    
     const createModalDiv = () => {
         const modalContent = document.createElement('div');
         modalContent.setAttribute('id', 'preapproval-form');
@@ -71,7 +71,9 @@ export default function (context) {
         modalContent.style.width = '100%';
         modalContent.style.height = '100%';
         modalContent.style.border = 'none';
-        modalContent.style.zIndex = '1001';
+        modalContent.style.zIndex = '1001';        
+        modalContent.style.backgroundColor = '#fff';  
+        modalContent.style.borderRadius = '.5rem';  
         modalContent.addEventListener('click', (e) => {
             console.log('clicked modal')
             e.preventDefault();
@@ -82,17 +84,81 @@ export default function (context) {
 
     const createModalHeader = () => {
         const header = document.createElement('div');
-        header.style.minHeight = '60px';
+        header.style.marginBottom = '0';
+        header.style.padding = '1rem 1.25rem';
+        header.style.borderBottom = '1px solid rgba(0,0,0,.125)';
         header.style.width = '100%';
-        header.style.backgroundColor = 'red';
+        header.style.backgroundColor = 'rgba(0,0,0,.03)';
+// Close Button
+        const closeButton = createCloseButton();
+        
+        const headerWrap = document.createElement('div');
+        headerWrap.style.display = 'flex';
+        headerWrap.style.justifyContent = 'space-between';
+        headerWrap.style.alignItems = 'flex-end';
+        headerWrap.style.webkitBoxPack = 'justify';
+        headerWrap.style.webkitBoxAlign = 'end';
+// Logo
+        const logoContainer = document.createElement('div');
+        logoContainer.style.width = '170px';
+        logoContainer.style.height = '70px';
+        logoContainer.style.display = 'flex';
+        logoContainer.style.alignItems = 'center';
+        logoContainer.style.justifyContent = 'flex-end';
+        const logo = document.createElement('img');
+        logo.setAttribute('class', 'header-logo-image');
+        logo.setAttribute('src', 'https://cdn11.bigcommerce.com/s-90vdngbq7j/images/stencil/150x60/buy-on-trust-logo_1563418732__08831.original.png');
+        logo.setAttribute('title', 'BuyOnTrust');
+        logo.setAttribute('alt', 'BuyOnTrust');
+        logoContainer.appendChild(logo);
+// Tag
+        const tag = document.createElement('div');
+        tag.style.color = '#004987';
+        tag.style.fontWeight = '700';
+        tag.style.fontFamily = 'Lato,sans-serif';
+        tag.style.fontSize = '15px';
+        const tagText = document.createTextNode('Buy On Trust');
+        tag.appendChild(tagText);
+
+// Construction
+        headerWrap.appendChild(logoContainer);
+        headerWrap.appendChild(tag);
+
+        header.appendChild(closeButton);
+        header.appendChild(headerWrap);
         return header;
+    };
+
+    const createCloseButton = () => {
+        const closeButton = document.createElement('button');
+        closeButton.setAttribute('class', 'close');
+        closeButton.style.padding = '0';
+        closeButton.style.backgroundColor = 'transparent';
+        closeButton.style.border = '0';
+        closeButton.style.float = 'right';
+        closeButton.style.fontSize = '1.5rem';
+        closeButton.style.fontWeight = '700';
+        closeButton.style.lineHeight = '1';
+        closeButton.style.color = '#000';
+        closeButton.style.textShadow = '0 1px 0 #fff';
+        closeButton.style.opacity = '.5';
+        closeButton.addEventListener('click', (e) => {
+            e.preventDefault();
+            const approvalModal = document.getElementById('preapproval-bg');
+            if (approvalModal) { approvalModal.remove(); }
+        })
+
+        const mark = document.createElement('span');
+        const markText = document.createTextNode('x');
+        mark.appendChild(markText);
+        closeButton.appendChild(mark);
+        return closeButton;
     };
 
     const createModalBody = () => {
         const body = document.createElement('div');
         body.style.minHeight = '400px';
         body.style.width = '100%';
-        body.style.backgroundColor = 'green';
         return body;
     };
 
